8ebf671a7f6c276ed7a9f61fb0b59df2a7b1f7a9
filipivanovic <filip_ivanovic@hotmail.com>
2025-05-27 13:23:14 +0200

    Update entry point to render App component
    Replaced the StarRating component with the App component as the main entry point in `index.js`. Commented out StarRating-related imports and configurations to prioritize the application structure.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import StarRating from './components/common/StarRating'
-// import './index.css'
-// import App from './App'
+// import StarRating from './components/common/StarRating'
+import './index.css'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    {/*<App />*/}
-    <StarRating
-      maxRating={5}
-      color="red"
-      size={24}
-      messages={['Terrible', 'Bad', 'Average', 'Good', 'Great']}
-    />
-    <StarRating maxRating={10} defaultRating={3} />
+      <App />
+    {/*<StarRating*/}
+    {/*  maxRating={5}*/}
+    {/*  color="red"*/}
+    {/*  size={24}*/}
+    {/*  messages={['Terrible', 'Bad', 'Average', 'Good', 'Great']}*/}
+    {/*/>*/}
+    {/*<StarRating maxRating={10} defaultRating={3} />*/}
   </React.StrictMode>
 )
