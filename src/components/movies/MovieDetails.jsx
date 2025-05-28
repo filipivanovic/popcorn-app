4a6190c4732f2d92e46852f1f6858afb27d6c9f6
filipivanovic <filip_ivanovic@hotmail.com>
2025-05-28 22:16:23 +0200

    Add movie details fetch with useEffect in MovieDetails
    Introduce an API call using `fetch` to retrieve movie details from OMDB based on the provided `id`. The useEffect hook ensures the data is fetched when the component is mounted.

--- src/components/movies/MovieDetails.jsx
+++ src/components/movies/MovieDetails.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from 'react'
+
+const KEY = 'fdc9b2b2'
+
 const MovieDetails = ({ id, onCloseMovie }) => {
+  useEffect(() => {
+    const getMovieDetails = async () => {
+      const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&i=${id}`)
+      const data = await res.json()
+      console.log(data)
+    }
+    getMovieDetails()
+  }, [])
+
   return (
     <div className="details">
       <button className={`btn-back`} onClick={onCloseMovie}>
