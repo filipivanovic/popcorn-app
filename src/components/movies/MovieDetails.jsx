0debb325da8e4954c2f6582d66f00499de717584
filipivanovic <filip_ivanovic@hotmail.com>
2025-05-28 22:30:14 +0200

    Enhance MovieDetails component with detailed movie overview
    Added state management to store movie data and fetch details dynamically based on movie ID. Introduced a detailed overview section displaying poster, title, release date, runtime, genre, IMDB rating, actors, and director. Updated useEffect to track changes in the ID prop for accurate data fetching.

--- src/components/movies/MovieDetails.jsx
+++ src/components/movies/MovieDetails.jsx
@@ -1,23 +1,40 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const KEY = 'fdc9b2b2'
 
 const MovieDetails = ({ id, onCloseMovie }) => {
+  const [movie, setMovie] = useState({})
+
   useEffect(() => {
     const getMovieDetails = async () => {
       const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&i=${id}`)
       const data = await res.json()
-      console.log(data)
+      setMovie(data)
     }
     getMovieDetails()
-  }, [])
+  }, [id])
 
   return (
     <div className="details">
-      <button className={`btn-back`} onClick={onCloseMovie}>
-        &larr;
-      </button>
-      <p>{id}</p>
+      <header>
+        <button className={`btn-back`} onClick={onCloseMovie}>
+          &larr;
+        </button>
+        <img src={movie.Poster} alt={movie.Title} />
+        <div className="details-overview">
+          <h2>{movie.Title}</h2>
+          <p>
+            {movie.Released} &bull; {movie.Runtime}
+          </p>
+          <p>{movie.Genre}</p>
+          <p>
+            <span>⭐</span>
+            {movie.imdbRating} IMDB Rating
+          </p>
+          <p>Starring: {movie.Actors}</p>
+          <p>Directed by {movie.Director}</p>
+        </div>
+      </header>
     </div>
   )
 }
